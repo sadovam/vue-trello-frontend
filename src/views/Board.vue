37ec28e870f30dc290648481dc4dcc50b021512a
Sadov Andrii <sadovam@gmail.com>
2021-04-19 18:17:03 +0300

    Add board fake data. Task 2 complete

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        HelloWorld
+        {{title}}
     </div>
 </template>
 
@@ -9,6 +9,37 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'Board',
+  data() {
+    return {
+      title: 'Моя тестовая доска',
+      lists: [
+        {
+          id: 1,
+          title: 'Планы',
+          cards: [
+            { id: 1, title: 'помыть кота' },
+            { id: 2, title: 'приготовить суп' },
+            { id: 3, title: 'сходить в магазин' },
+          ],
+        },
+        {
+          id: 2,
+          title: 'В процессе',
+          cards: [
+            { id: 4, title: 'посмотреть сериал' },
+          ],
+        },
+        {
+          id: 3,
+          title: 'Сделано',
+          cards: [
+            { id: 5, title: 'сделать домашку' },
+            { id: 6, title: 'погулять с собакой' },
+          ],
+        },
+      ],
+    };
+  },
 });
 </script>
 
