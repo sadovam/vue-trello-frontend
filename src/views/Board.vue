3e77599806cf39601ea661d062d1c1af15617138
Sadov Andrii <sadovam@gmail.com>
2021-04-19 17:53:55 +0300

    Add Board page

---
+++ src/views/Board.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        HelloWorld
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Board',
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
